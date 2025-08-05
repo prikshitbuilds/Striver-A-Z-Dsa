7abfe0ed103e03a96b486b997a7bb5ae64fd3899
PrikshitBuilds <prikshitbuilds@gmail.com>
2025-08-05 18:05:48 +0530

    Create bfs.cpp

---
+++ Graphs/bfs.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  public:
+    // Function to return Breadth First Traversal of given graph.
+    vector<int> bfs(vector<vector<int>> &adj) {
+        // Code here
+        int V = adj.size();
+        vector<int>vis(V,0);
+      
+        
+        queue<int>q;
+        q.push(0);
+          vis[0]=1;
+        vector<int>bfs;
+        
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            bfs.push_back(node);
+            
+            for(auto it : adj[node]){
+                if(!vis[it]){
+                    vis[it]=1;
+                    q.push(it);
+                }
+            }
+        }
+        return bfs;
+    }
+};
